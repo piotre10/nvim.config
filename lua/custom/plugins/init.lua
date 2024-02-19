2f3d1d1fa7b6ba7a9049414ccb963de142e8c60c
Piotr <piotr.luka@tutanota.com>
2024-02-19 13:20:51 +0100

    Add additional plugins, for python venv selection and markdown preview

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    enabled = true,
+    ft = "markdown",
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    ft = "python",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-telescope/telescope.nvim"
+    },
+    config=true,
+    event="VeryLazy",
+  },
+}
